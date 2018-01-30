0a782eb183f7e365322707f5382745926eeec27b
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2018-01-30 23:00:59 +0530

    displaying messages using FirebaseUI

--- app/src/main/java/com/example/cheshta/chatapp/MainActivity.java
+++ app/src/main/java/com/example/cheshta/chatapp/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.cheshta.chatapp;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.cheshta.chatapp.Model.Message;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText etMessage;
     DatabaseReference mDatabase;
+    RecyclerView rvMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +25,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         etMessage = findViewById(R.id.etMessage);
+        rvMessage = findViewById(R.id.rvMessage);
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Messages");
+
+        rvMessage.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        rvMessage.setLayoutManager(linearLayoutManager);
     }
 
     public void sendButtonClicked(View view){
@@ -32,6 +39,39 @@ public class MainActivity extends AppCompatActivity {
         if (!messageValue.isEmpty()){
             final DatabaseReference newPost = mDatabase.push();
             newPost.child("content").setValue(messageValue);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Message, MessageViewHolder> FBRA = new FirebaseRecyclerAdapter<Message, MessageViewHolder>(
+                Message.class,
+                R.layout.single_message_layout,
+                MessageViewHolder.class,
+                mDatabase
+        ) {
+            @Override
+            protected void populateViewHolder(MessageViewHolder viewHolder, Message model, int position) {
+                viewHolder.setContent(model.getContent());
+            }
+        };
+        rvMessage.setAdapter(FBRA);
+    }
+
+    public static class MessageViewHolder  extends RecyclerView.ViewHolder{
+
+        View mView;
+
+        public MessageViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setContent(String content){
+            TextView tvMessage = mView.findViewById(R.id.tvMessage);
+            tvMessage.setText(content);
         }
     }
 }
